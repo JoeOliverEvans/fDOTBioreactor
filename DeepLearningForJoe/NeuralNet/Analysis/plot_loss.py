d8c643a607a2cfaaf5b61add7727c2b65048c3db
joeol <joeoliverevans@gmail.com>
2024-07-04 17:40:05 +0100

    finished simple plot for testing and training loss

--- DeepLearningForJoe/NeuralNet/Analysis/plot_loss.py
+++ DeepLearningForJoe/NeuralNet/Analysis/plot_loss.py
@@ -1,20 +1,31 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import mat73
+import scipy.io
 
 
 def plot_loss(data_string):
-    data = mat73.loadmat(data_string)
-    training_loss = data['training_loss']
-    testing_loss = data['testing_loss']
+    data = scipy.io.loadmat(data_string)
+    training_loss = list(data['training_loss'].T)
+    testing_loss = list(data['testing_loss'].T)
 
-    plt.plot(x=np.linspace(0, len(training_loss)), y=training_loss, label='training loss', color='red')
-    plt.plot(x=np.linspace(0, len(testing_loss)), y=testing_loss, label='testing loss', color='blue')
-    plt.title('Testing and training loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
+    fig, ax1 = plt.subplots()
+
+    colour1 = 'tab:red'
+    ax1.set_title('Testing and training loss')
+    ax1.set_xlabel('Epochs')
+    ax1.plot(np.arange(0, len(training_loss)), training_loss, label='training loss', color=colour1)
+    ax1.tick_params(axis='y', labelcolor=colour1)
+    ax1.set_ylabel('Training Loss')
+
+    ax2 = ax1.twinx()
+    colour2 = 'tab:blue'
+    ax2.plot(np.arange(0, len(testing_loss)), testing_loss, label='testing loss', color=colour2)
+    ax2.set_ylabel('Testing Loss', color=colour2)
+    ax2.tick_params(axis='y', labelcolor=colour2)
+    fig.tight_layout()
     plt.show()
 
 
-file = r'C:\Joe Evans\University\Computing\Summer Project\DeepLearningForJoe\NeuralNet\_Dummy\test_processed.mat'
+file = r'C:\Joe Evans\University\Computing\Summer Project\DeepLearningForJoe\NeuralNet\Datasets\Blob\Blob_10_1\loss_3D_UNet3.mat'
+plot_loss(file)
