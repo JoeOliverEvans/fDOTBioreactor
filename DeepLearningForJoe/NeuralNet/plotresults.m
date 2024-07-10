543b27ccfcd7361e4d9785262cf4ee151c2129de
joeol <joeoliverevans@gmail.com>
2024-07-10 13:01:21 +0100

    now plots both truth and recon1 at the same time

--- DeepLearningForJoe/NeuralNet/plotresults.m
+++ DeepLearningForJoe/NeuralNet/plotresults.m
@@ -2,7 +2,8 @@ clear
 %load('test_processed.mat')
 % load('../SimData/3D/images3.mat')
 %load('../SimulateData/images3_cylinder.mat')
-load('Datasets/blob10/images3_blobs10.mat')
+%load('Datasets/Gaussian/Gaussian_20_1/test_processed.mat')
+load('Datasets/Gaussian/images3_gaussian3_tester10.mat')
 load('bnd.mat')
 
 recon1 = noisy_img(:,:,:,1:end);
@@ -11,10 +12,12 @@ clear noisy_img clean_img
 
 cmap = hot(128); % colormap
 
-thresh = 0; % Since our data is normalized, this effectively says we only care about voxels above one standard deviation
+thresh = 1; % Since our data is normalized, this effectively says we only care about voxels above one standard deviation
 
 %%%% Change this to something you need
-tmp=truth(:,:,:,2);
+i = 1;
+tmp=recon1(:,:,:,i);
+tmp2=truth(:,:,:,i);
 %%%%
 
 tmp(tmp<thresh) = 0;
@@ -27,3 +30,11 @@ cfg1.BackgroundColor = [1,1,1];
 cfg1.BackgroundGradient = 0;
 
 
+tmp2(tmp2<thresh) = 0;
+% bnd is the boundary of the cylinder. Added only for nicer visualization
+vol2 = volshow(tmp2+bnd, Colormap=cmap); %tmp+bnd
+cfg2 = vol2.Parent;
+cfg2.CameraPosition=[0,-120,80];
+cfg2.CameraUpVector=[0,0,1];
+cfg2.BackgroundColor = [1,1,1];
+cfg2.BackgroundGradient = 0;
